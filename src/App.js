bae59750ef1413f802253cd5cd9a3301c339eb21
dhohirpradana <dhohirkudus@gmail.com>
2022-04-04 06:23:00 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <>
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <Navbar.Brand>Dhohir Pradana</Navbar.Brand>
+                <Nav className="me-auto">
+                  <Link className="nav nav-item nav-link active" to="/">
+                    Home
+                  </Link>
+                </Nav>
+              </Container>
+            </Navbar>
+          </>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,126 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-unused-vars */
+import React, { useRef, useState } from "react";
+import { Form, Button, Alert, Spinner } from "react-bootstrap";
+
+export default function Home() {
+  const ref = useRef();
+
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [spinner, setSpinner] = useState(null);
+
+  async function handleSubmit() {
+    setSpinner(true);
+    setError(null);
+    const inputs = ref.current;
+    const accessToken = inputs.accessToken.value;
+    const count = inputs.count.value;
+    const delay = inputs.delay.value;
+    const targetLink = inputs.targetLink.value;
+    if (
+      [accessToken, count, delay, targetLink].some((el) => el != "" || el != 0)
+    ) {
+      for (let index = 0; index < count; index++) {
+        fetch(
+          `https://graph.facebook.com/me/feed?link=${targetLink}&published=false&access_token=${accessToken}&fields=id`,
+          {
+            method: "POST",
+          }
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            setSpinner(null);
+            if (json.error) {
+              setError(json.error.message);
+              return;
+            } else {
+              setSuccess(index);
+            }
+          })
+          .catch((err) => {
+            setError(err);
+          });
+        await setTimeout(() => {}, delay);
+      }
+      setSuccess(null);
+    }
+  }
+
+  return (
+    <div className="text-light px-5 pt-3">
+      <div className="mx-5 px-4">
+        {error ? (
+          <Alert variant="danger" onClose={() => setError(null)} dismissible>
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>{error}</p>
+          </Alert>
+        ) : (
+          <div></div>
+        )}
+        {spinner ? <Spinner animation="border" /> : <div></div>}
+        {success ? (
+          <Alert variant="success">
+            <Alert.Heading>{success}</Alert.Heading>
+            <p>Processing...</p>
+          </Alert>
+        ) : (
+          <div></div>
+        )}
+        <Form ref={ref}>
+          <div>
+            <Form.Group className="mb-3" controlId="access-token">
+              <Form.Label>Access Token</Form.Label>
+              <Form.Control
+                type="text"
+                name="accessToken"
+                placeholder="Enter access token"
+              />
+            </Form.Group>
+          </div>
+          <div>
+            <Form.Group className="mb-3" controlId="count">
+              <Form.Label>Jumlah</Form.Label>
+              <Form.Control
+                type="number"
+                name="count"
+                min={1}
+                max={1000}
+                placeholder="Enter share count"
+              />
+            </Form.Group>
+          </div>
+          <div>
+            <Form.Group className="mb-3" controlId="delay">
+              <Form.Label>Delay</Form.Label>
+              <Form.Control
+                type="number"
+                min={1}
+                max={10}
+                name="delay"
+                placeholder="Enter share delay"
+              />
+            </Form.Group>
+          </div>
+          <div>
+            <Form.Group className="mb-3" controlId="target-link">
+              <Form.Label>Target Link</Form.Label>
+              <Form.Control
+                type="text"
+                name="targetLink"
+                placeholder="Enter target link"
+              />
+            </Form.Group>
+            <Button
+              onClick={handleSubmit}
+              style={{ width: "100%" }}
+              variant="success"
+            >
+              Share Now
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
